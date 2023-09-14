d276dbbe2c3f6f1b0bb627ab29a8587468e1e14c
khenfer salah eddine <107678289+salahkhenfer@users.noreply.github.com>
2023-09-14 22:36:10 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 from flask import Flask, render_template, request
 from keras.models import load_model
 from keras.preprocessing import image
-import cv2
+from PIL import Image
+
 import numpy as np
 
 app = Flask(__name__)
-
 
 dic = {
     0: " تشير إلى حالة أو مستوى عادي أو طبيعي دون وجود مشكلة كبيرة.",
@@ -15,7 +15,6 @@ dic = {
     4: " تشير إلى حالة أو مستوى شديد أو كبير من مشكلة معينة، وغالبًا ما يكون له تأثير كبير ويتطلب اهتمامًا وعلاجًا فوريً",
 }
 
-
 # Image Size
 img_size = 256
 model = load_model("model.h5")
@@ -24,14 +23,13 @@ model.make_predict_function()
 
 
 def predict_label(img_path):
-    img = cv2.imread(img_path, cv2.IMREAD_GRAYSCALE)
-    resized = cv2.resize(img, (img_size, img_size))
-    i = resized / 255.0
-    i = i.reshape(1, img_size, img_size, 1)
-    probabilities = model.predict(i)[0]
-    predicted_class = dic[
-        int(np.argmax(probabilities))
-    ]  # Determine the class with the highest probability
+    img = Image.open(img_path).convert("L")  # Open image in grayscale mode
+    img = img.resize((img_size, img_size))
+    img_array = np.array(img)
+    img_array = img_array / 255.0
+    img_array = img_array.reshape(1, img_size, img_size, 1)
+    probabilities = model.predict(img_array)[0]
+    predicted_class = dic[int(np.argmax(probabilities))]
     return predicted_class
 
 
@@ -53,5 +51,4 @@ def upload():
 
 
 if __name__ == "__main__":
-    # app.debug = True
     app.run(debug=True)
